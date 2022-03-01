d29b27f5f03f10d8ba2f837542a41a8f3acd2331
9Pattarapon <pptrpw123@gmail.com>
2022-03-02 03:11:55 +0700

    lab_06

---
+++ routes/years_route.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from models.years_model import Years
+import datetime
+
+y = datetime.datetime.now()
+years_api_router = APIRouter()
+
+@years_api_router.get("/service/getage")
+async def get_test(year:int):
+
+    if year <= 0 :
+        return {"age": "years not less than 0"}
+    elif year > (y.year + 543) :
+        return {"age": "unable to calculate"}
+    else:
+        age = (y.year + 543) - year
+        return {"age": age}
---
+++ tests/test_years.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+import sys        
+sys.path.insert(0, '../lab_test06')        
+from main import app
+# import datetime
+
+# now = datetime.datetime.now()
+client = TestClient(app)
+
+def test_test_input_year_api():
+    input = "2540"
+    output = 25
+    response = client.get("/service/getage?year="+input)
+    assert response.status_code == 200
+    assert response.json() == {"age": output}
+
+def test_test_input_zero_api():
+    input = "0"
+    output = "years not less than 0"
+    response = client.get("/service/getage?year="+input)
+    assert response.status_code == 200
+    assert response.json() == {"age": output}
+
+def test_test_year_unable_calculate_api():
+    input = "2566"
+    output = "unable to calculate"
+    response = client.get("/service/getage?year="+input)
+    assert response.status_code == 200
+    assert response.json() == {"age": output}
+
+def test_test_year_Underflow_api():
+    input = "-99"
+    output = "years not less than 0"
+    response = client.get("/service/getage?year="+input)
+    assert response.status_code == 200
+    assert response.json() == {"age": output}
+
+   
